9f3d8ee24c21f47047ada34196f7eed200089275
LUPOSaymon <mija.1895592@studenti.uniroma1.it>
2020-03-18 14:47:49 +0100

    -Added Exit condition -Fixes DIMENSIONE variable

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,41 +10,45 @@ public class Main
 
     public static void main(String[] args)
     {
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Inserisci il nome dell'immagine caricata nella cartella Images(senza estensione):\n");
-        String imageName = scanner.next();
-        String fileName = "Images/" + imageName + ".png";
+        String imageName = "";
+        while(true) {
+            System.out.println("Inserisci il nome dell'immagine caricata nella cartella Images(senza estensione) (scrivere .exit per terminare l'esecuzione):");
+            imageName = scanner.next();
+            if (imageName.equals(".exit"))
+                break;
+            String fileName = "Images/" + imageName + ".png";
 
 
-        try
-        {
-            BufferedImage image = ImageIO.read(new File(fileName));
-            FileWriter myWriter = new FileWriter("Files/" + imageName+ ".txt");
-            myWriter.write("public static final int DIMENSIONE = 10;\n" +
-                                "public static boolean[][] mkStruct (int d){\n" +
-                                "   boolean[][] m = new boolean[d][d];{\n");
-            for (int y = 0; y < image.getHeight(); y++)
-            {
-                for (int x = 0; x  < image.getWidth(); x++)
-                {
-                    int rgb = image.getRGB(x,y);
-                    int  red   = (rgb & 0x00ff0000) >> 16;
-                    int  green = (rgb & 0x0000ff00) >> 8;
-                    int  blue  =  rgb & 0x000000ff;
+            try {
+                BufferedImage image = ImageIO.read(new File(fileName));
+                int dimensions = image.getHeight();
+                FileWriter myWriter = new FileWriter("Files/" + imageName + ".txt");
+                myWriter.write("public static final int DIMENSIONE = " + dimensions + ";\n" +
+                        "public static boolean[][] mkStruct (int d){\n" +
+                        "   boolean[][] m = new boolean[d][d];{\n");
+                for (int y = 0; y < image.getHeight(); y++) {
+                    for (int x = 0; x < image.getWidth(); x++) {
+                        int rgb = image.getRGB(x, y);
+                        int red = (rgb & 0x00ff0000) >> 16;
+                        int green = (rgb & 0x0000ff00) >> 8;
+                        int blue = rgb & 0x000000ff;
 
-                    if(red == 0 && green == 0 && blue == 0)
-                        myWriter.write("    m["+ y +"][" + x + "] = false;\n");
-                    else
-                        myWriter.write("    m["+ y +"][" + x + "] = true;\n");
+                        if (red == 0 && green == 0 && blue == 0)
+                            myWriter.write("    m[" + y + "][" + x + "] = false;\n");
+                        else
+                            myWriter.write("    m[" + y + "][" + x + "] = true;\n");
 
+                    }
                 }
+                myWriter.write("    }return m;\n" +
+                        "}");
+                myWriter.close();
+                System.out.println("File creato con successo :D");
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
             }
-            myWriter.write("    }return m;\n" +
-                                "}");
-            myWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("File Non trovato!");
         }
     }
 }
